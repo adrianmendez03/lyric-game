a2279279c272e8cd19a28944dacae208488c8ebd
Adrian Mendez <adrian@Adrians-MacBook-Air.local>
2021-01-15 14:40:26 -0600

    React Router configured

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import Source from './pages/Source'
+import Search from './pages/Search'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/source">
+          <Source />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
     </div>
   );
 }
