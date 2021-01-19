528f7d7c837fa48bde86d48f1733807d5090a31e
Adrian Mendez <adrian@Adrians-MacBook-Air.local>
2021-01-18 22:58:55 -0600

    Mobile menu configured

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
+
+    const [textInput, setTextInput] = useState("")
+
+    const handleChange = event => {
+        setTextInput(event.target.value)
+    }
+
     return (
-        <div>Links</div>
+        <>
+            <div className="links-container">
+                <Link to="/">Home</Link>
+                <Link to="/sources">Sources</Link>
+            </div>
+            <div className="search-container">
+                <i className="fas fa-search"></i>
+                <input type="text" name="search" value={textInput} onChange={handleChange}/>
+            </div>
+        </>
     )
 }
 
